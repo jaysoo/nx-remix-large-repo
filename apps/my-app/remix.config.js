d49ecf33f05b2b442841e1ac2a7fdd6b55df910e
Jack Hsu <jack.hsu@gmail.com>
2023-05-17 21:30:34 -0400

    fix createWatchPaths function

--- apps/my-app/remix.config.js
+++ apps/my-app/remix.config.js
@@ -1,10 +1,12 @@
-function createWatchPaths() {
-  const { workspaceLayout, workspaceRoot } = require('@nx/devkit');
-  const { join } = require('path');
-  return [
-    join(workspaceRoot, workspaceLayout().libsDir),
-    join(workspaceRoot, 'packages'),
-  ];
+function createWatchPaths(dirname) {
+  const { workspaceLayout, workspaceRoot, offsetFromRoot } = require('@nx/devkit');
+  const { relative, join } = require('path');
+  const offset = offsetFromRoot(relative(workspaceRoot, dirname));
+  const { libsDir } = workspaceLayout();
+  return libsDir === 'packages'
+    ? [join(offset, libsDir)]
+    : [join(offset, libsDir), join(offset, 'packages')];
+
 }
 /**
 
@@ -16,5 +18,7 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  watchPaths: createWatchPaths(),
+  watchPaths: createWatchPaths(__dirname),
 };
+
+console.log(module.exports.watchPaths);
